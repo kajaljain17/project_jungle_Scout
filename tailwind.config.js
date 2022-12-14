c718c9a1718840d924cd37487f9d2677d7d753bc
kajaljain17 <jain.kajal891@gmail.com>
2022-12-15 05:01:37 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'theme-colour': '#f47820',
+        'section-colour':'#f9f9fa',
+      },
+    },
+  },
+  plugins: [],
+}
